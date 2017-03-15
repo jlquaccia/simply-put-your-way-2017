f23ae46be5b200754e7eeffdf9723f961065b99e
Jason Quaccia <jlquaccia@gmail.com>
2017-03-15 13:01:03 -0700

    first draft of post model has working crud actions on backend

--- app/models/post.js
+++ app/models/post.js
@@ -4,5 +4,6 @@ var mongoose = require('mongoose');
 // define our post model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Post', {
-  title: {type: String, default: ''}
+  title: {type: String, default: ''},
+  body: {type: String}
 });
--- app/routes.js
+++ app/routes.js
@@ -1,24 +1,83 @@
 // grab the post model we just created
-var Post = require('./models/post');
+var Post = require('./models/Post');
 
 module.exports = function (app) {
   // server routes ===========================================================
-  // handle things like api calls
-  // authentication routes
-
-  // sample api route
+  // get all posts
   app.get('/api/posts', function (req, res) {
-    // use mongoose to get all posts in the database
     Post.find(function (err, posts) {
-      // if there is an error retrieving, send the error. 
-      // nothing after res.send(err) will execute
-      if (err) res.send(err);
-      res.json(posts); // return all posts in JSON format
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(posts);
+      }
     });
   });
 
-  // route to handle creating goes here (app.post)
-  // route to handle delete goes here (app.delete)
+  // get one specific post
+  app.get('/api/posts/:id', function (req, res) {
+    Post.findOne({_id: req.params.id}, function (err, post) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(post);
+      }
+    });
+  });
+
+  // add a new post
+  app.post('/api/posts', function (req, res) {
+    var newData = {
+      title: req.body.title,
+      body: req.body.body
+    };
+
+    var newPost = new Post(newData);
+
+    newPost.save(function (err, newPost) {
+      if (err) {
+        res.status(500);
+        res.json({status: 500, error: err});
+        res.end();
+      } else {
+        res.json({status: 200, newPost: newPost});
+        res.end();
+      }
+    });
+  });
+
+  // update one specific post
+  app.put('/api/posts/:id', function (req, res) {
+    var updateData = {
+      title: req.body.title,
+      body: req.body.body
+    };
+
+    Post.update({_id: req.params.id}, updateData, function (err, post) {
+      if (err) {
+        res.status(500);
+        res.json({status: 500, error: err});
+        res.end();
+      } else {
+        res.json({status: 200, post: post});
+        res.end();
+      }
+    });
+  });
+
+  // delete one specific post
+  app.delete('/api/posts/:id', function (req, res) {
+    Post.remove({_id: req.params.id}, function (err, post) {
+      if (err) {
+        res.status(500);
+        res.json({status: 500, error: err});
+        res.end();
+      } else {
+        res.json({status: 200, message: 'post was successfully deleted'});
+        res.end();
+      }
+    });
+  });
 
   // frontend routes =========================================================
   // route to handle all angular requests
