fdc69020f12954ab0c1a2ae4b4636154506b8409
Jason Quaccia <jlquaccia@gmail.com>
2017-03-29 12:05:42 -0700

    added scroll to top button

--- public/js/controllers/NavbarCtrl.js
+++ public/js/controllers/NavbarCtrl.js
@@ -1,9 +1,27 @@
 angular
   .module('simply-put-your-way')
   .controller('NavbarCtrl', ['$scope', function ($scope) {
+    // show scroll to top button after user has scrolled down the page
+    $(window).scroll(function () {
+      var scrollPos = $(window).scrollTop();
+
+      if (scrollPos > 0) {
+        $('.navWrapper .scrollTop').addClass('show-button');
+      } else {
+        $('.navWrapper .scrollTop').removeClass('show-button');
+      }
+    });
+      
     // toggle mobile overlay
     $('.button a').click(function () {
       $('.overlay').fadeToggle(200);
       $(this).toggleClass('btn-open').toggleClass('btn-close');
     });
+
+    // scroll to top of page button
+    $scope.scrollToTop = function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+    };
   }]);
