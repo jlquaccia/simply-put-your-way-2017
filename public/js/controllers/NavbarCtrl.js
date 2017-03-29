88f5a9d7c1a24df96cc595b876aae8a90f83b36b
Jason Quaccia <jlquaccia@gmail.com>
2017-03-29 13:09:12 -0700

    set navbar to fixed and can now hide/show on scroll

--- public/js/controllers/NavbarCtrl.js
+++ public/js/controllers/NavbarCtrl.js
@@ -1,15 +1,32 @@
 angular
   .module('simply-put-your-way')
   .controller('NavbarCtrl', ['$scope', function ($scope) {
-    // show scroll to top button after user has scrolled down the page
+    // handles events on scroll
+    var lastScrollTop = 0;
+
     $(window).scroll(function () {
       var scrollPos = $(window).scrollTop();
+      var currentScroll = $(this).scrollTop();
 
+      // show/hide scroll to top button when necessary
       if (scrollPos > 0) {
         $('.navWrapper .scrollTop').addClass('show-button');
       } else {
         $('.navWrapper .scrollTop').removeClass('show-button');
       }
+
+      // show/hide navbar when necessary
+      if (currentScroll > lastScrollTop) {
+        console.log('downscrolled');
+        $('.navWrapper .headroom').removeClass('headroom--pinned');
+        $('.navWrapper .headroom').addClass('headroom--unpinned');
+      } else {
+        console.log('upscrolled');
+        $('.navWrapper .headroom').removeClass('headroom--unpinned');
+        $('.navWrapper .headroom').addClass('headroom--pinned');
+      }
+
+      lastScrollTop = currentScroll;
     });
       
     // toggle mobile overlay
