58803939e07d0a66b614e2c8195b4bc33e0ca249
Jason Quaccia <jlquaccia@gmail.com>
2017-04-01 09:04:59 -0700

    finished integrating pinterest api into the site.  able to get pinterest data to display in the view

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,6 +1,6 @@
 angular
   .module('simply-put-your-way')
-  .controller('HomeCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+  .controller('HomeCtrl', ['$scope', 'parallaxHelper', 'Pinterest', function ($scope, parallaxHelper, Pinterest) {
     // parallax
     $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
     $scope.spacerImage = parallaxHelper.createAnimator(-0.3,150,-150);
@@ -13,4 +13,20 @@ angular
       prevNextButtons: false,
       autoPlay: 5000
     };
+
+    // getting pinterest data
+    // board: its your space
+    Pinterest.getBoardPins('490329546869188172').then(function (response) {
+      $scope.itsYourSpace = response.data;
+    });
+
+    // board: repurpose it
+    Pinterest.getBoardPins('490329546869301849').then(function (response) {
+      $scope.repurposeIt = response.data;
+    });
+
+    // board: redecorate
+    Pinterest.getBoardPins('490329546869301857').then(function (response) {
+      $scope.redecorate = response.data;
+    });
   }]);
---
+++ public/js/services/Pinterest.js
@@ -0,0 +1,42 @@
+angular
+  .module('simply-put-your-way')
+  .factory('Pinterest', ['$q', function ($q) {
+    PDK.init({appId: "4892673736834035409", cookie: true});
+
+    function login (callback) {
+      PDK.login({scope: 'read_public'}, callback);
+    }
+
+    function logout () {
+      PDK.logout();
+    }
+
+    function isLoggedIn () {
+      return !!PDK.getSession();
+    }
+
+    function getBoards (callback) {
+      PDK.me('boards', {fields: 'id,name,url,description,creator,created_at,counts,image'}, callback);
+    }
+
+    function getBoardPins (id) {
+      // wrap request in an angular promise so you make sure the response has resolved before passing it over (ex. to a controller etc.)
+      return $q(function (resolve, reject) {
+        PDK.request('/v1/boards/' + id + '/pins/', 'GET', {fields: 'id,link,url,creator,board,created_at,note,color,counts,media,attribution,image,metadata'}, function (response) {
+          if (response) {
+            resolve(response);
+          }
+
+          reject();
+        });
+      });
+    }
+
+    return {
+      login: login,
+      logout: logout,
+      isLoggedIn: isLoggedIn,
+      getBoards: getBoards,
+      getBoardPins: getBoardPins
+    };
+  }]);
