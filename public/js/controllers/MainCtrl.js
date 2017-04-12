0747f16181c2fcc69f23796eb2363a2025803d2b
Jason Quaccia <jlquaccia@gmail.com>
2017-04-12 12:21:39 -0700

    fixed excess border bottom problem on mobile overlay

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,7 @@
 angular
   .module('simply-put-your-way')
   .controller('MainCtrl', ['$rootScope', function ($rootScope) {
+    // make sure window is always scrolled to the top before state change
     $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
       $('html, body').animate({
         scrollTop: 0
