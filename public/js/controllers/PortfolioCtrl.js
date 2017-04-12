978384994ba6a449910283095729363e32069089
Jason Quaccia <jlquaccia@gmail.com>
2017-04-12 16:05:25 -0700

    lots of progress on portfolio page

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,5 +1,5 @@
 angular
   .module('simply-put-your-way')
-  .controller('PortfolioCtrl', ['$scope', function ($scope) {
-    $scope.test = 'portfolio';
+  .controller('PortfolioCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+    $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
   }]);
