5685a3d8d81ab8d1aeb4b796656256bef5fc9974
Jason Quaccia <jlquaccia@gmail.com>
2017-04-12 14:34:02 -0700

    built out content for about page

--- public/js/controllers/AboutCtrl.js
+++ public/js/controllers/AboutCtrl.js
@@ -1,5 +1,6 @@
 angular
   .module('simply-put-your-way')
-  .controller('AboutCtrl', ['$scope', function ($scope) {
-    $scope.test = 'about';
+  .controller('AboutCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+    // parallax
+    $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
   }]);
