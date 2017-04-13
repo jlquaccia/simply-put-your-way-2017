5cad5f015aa908befdd509a158b1cdcbef6483a1
Jason Quaccia <jlquaccia@gmail.com>
2017-04-13 13:53:22 -0700

    added storage ideas service and integrated it into the portfolio page

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,5 +1,9 @@
 angular
   .module('simply-put-your-way')
-  .controller('PortfolioCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+  .controller('PortfolioCtrl', ['$scope', 'parallaxHelper', 'StorageIdeas', function ($scope, parallaxHelper, StorageIdeas) {
+    // parallax
     $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
+
+    // get storage ideas
+    $scope.storageIdeas = StorageIdeas;
   }]);
