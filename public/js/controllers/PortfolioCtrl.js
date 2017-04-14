7debf11d223b96743583d029d5a541fff8b9235a
Jason Quaccia <jlquaccia@gmail.com>
2017-04-13 18:18:58 -0700

    setup lightbox functionality for storage ideas overlay

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,6 +1,8 @@
 angular
   .module('simply-put-your-way')
   .controller('PortfolioCtrl', ['$scope', 'parallaxHelper', 'StorageIdeas', function ($scope, parallaxHelper, StorageIdeas) {
+    var currentStorageIdeaIndex = null;
+
     // parallax
     $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
 
@@ -9,12 +11,35 @@ angular
 
     // toggle overlay
     $(document).ready(function () {
-      $('#portfolio .storageIdeas .innerWrapper').click(function () {
-        $('#portfolio .overlay-scale').addClass('storageIdeasOverlayOpen');
-      });
+      $scope.toggleOverlay = function () {
+        $('#portfolio .overlay-scale').toggleClass('storageIdeasOverlayOpen');
+      };
 
-      $('#portfolio .overlay .overlay-close').click(function () {
-        $('#portfolio .overlay-scale').removeClass('storageIdeasOverlayOpen');
-      });
+      $scope.setCurrentStorageIdea = function (index) {
+        $scope.currentStorageIdea = StorageIdeas[index];
+        currentStorageIdeaIndex = index;
+      };
+
+      $scope.next = function () {
+        if (currentStorageIdeaIndex < StorageIdeas.length - 1) {
+          currentStorageIdeaIndex++;
+          console.log(currentStorageIdeaIndex);
+          $scope.currentStorageIdea = StorageIdeas[currentStorageIdeaIndex];
+        } else {
+          currentStorageIdeaIndex = 0;
+          $scope.currentStorageIdea = StorageIdeas[currentStorageIdeaIndex];
+        }
+      };
+
+      $scope.prev = function () {
+        if (currentStorageIdeaIndex > 0) {
+          currentStorageIdeaIndex--;
+          console.log(currentStorageIdeaIndex);
+          $scope.currentStorageIdea = StorageIdeas[currentStorageIdeaIndex];
+        } else {
+          currentStorageIdeaIndex = StorageIdeas.length - 1;
+          $scope.currentStorageIdea = StorageIdeas[currentStorageIdeaIndex];
+        }
+      };
     });
   }]);
