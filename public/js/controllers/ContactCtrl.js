d428410dc6de7fb6a0a35ffe748e2e09bda19949
Jason Quaccia <jlquaccia@gmail.com>
2017-04-16 12:20:05 -0700

    contact page in progress

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,5 +1,6 @@
 angular
   .module('simply-put-your-way')
-  .controller('ContactCtrl', ['$scope', function ($scope) {
-    $scope.test = 'contact';
+  .controller('ContactCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+    // parallax
+    $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
   }]);
