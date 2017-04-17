9fbc314308b17c8717d22924896aaf4a45d45a4c
Jason Quaccia <jlquaccia@gmail.com>
2017-04-17 12:01:39 -0700

    setup structure on client side to handle sending contact form messages

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,14 +1,33 @@
 angular
   .module('simply-put-your-way')
-  .controller('ContactCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
+  .controller('ContactCtrl', ['$scope', '$http', 'parallaxHelper', function ($scope, $http, parallaxHelper) {
     // parallax
     $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
 
     // send message
     $scope.sendMessage = function (form) {
       if (form.$valid) {
-        console.log('form is valid');
-        console.log($scope.contactForm);
+        // console.log('form is valid');
+        
+        var data = ({
+          name: this.cf.name,
+          email: this.cf.email,
+          subject: this.cf.subject,
+          message: this.cf.message
+        });
+
+        // console.log(data);
+
+        $http.post('/contact-form', data)
+          .then(
+            function (data, status, headers, config) {
+              // show material design toast here on success
+              console.log('success');
+            },
+            function (data, status, headers, config) {
+              console.log('something went wrong');
+            }
+          );
       } else {
         console.log('form is not valid');
       }
