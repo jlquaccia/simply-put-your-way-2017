c07a68536aaae0461de3b3f281af8b530079d7ad
Jason Quaccia <jlquaccia@gmail.com>
2017-04-17 11:21:51 -0700

    set up frontend validations for contact form

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -3,4 +3,14 @@ angular
   .controller('ContactCtrl', ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
     // parallax
     $scope.background = parallaxHelper.createAnimator(-0.3,150,0);
+
+    // send message
+    $scope.sendMessage = function (form) {
+      if (form.$valid) {
+        console.log('form is valid');
+        console.log($scope.contactForm);
+      } else {
+        console.log('form is not valid');
+      }
+    };
   }]);
