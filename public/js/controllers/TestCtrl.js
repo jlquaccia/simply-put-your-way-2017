df5cc755aff53548d6a635473236b372ada4930f
Jason Quaccia <jlquaccia@gmail.com>
2017-04-22 14:11:46 -0700

    solved flickity problem once and for all

--- public/js/controllers/TestCtrl.js
+++ public/js/controllers/TestCtrl.js
@@ -1,18 +1,5 @@
 angular
   .module('simply-put-your-way')
   .controller('TestCtrl', ['$scope', function ($scope) {
-    $scope.slides = [
-      'http://bnj.bz/2p2S1X471H38/one.jpg',
-      'http://bnj.bz/352I0q311A23/two.jpg',
-      'http://bnj.bz/3j1J1h1s2o3p/three.jpg',
-      'http://bnj.bz/0i0l442Z433Y/four.jpg',
-    ];
-
-    $scope.flickityOptions = {
-      cellSelector: '.demo__slide',
-      resize: false,
-      setGallerySize: false,
-      friction: .4,
-      selectedAttraction: .1
-    };
+    
   }]);
