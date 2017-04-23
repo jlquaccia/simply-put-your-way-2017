8148eaf30d3c1056b50813059fa1923283db5bbf
Jason Quaccia <jlquaccia@gmail.com>
2017-04-22 17:37:13 -0700

    shrunk down all large imgs

--- public/js/services/ImagePreloaderService.js
+++ public/js/services/ImagePreloaderService.js
@@ -3,13 +3,13 @@ angular
   .factory('imagePreloader', [function () {
     function headStart () {
       var imgArray = [
-        {img: '../images/new/Rainbow Like with Like.jpg', img2: 'url("../images/new/Rainbow Like with Like.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#about'},
+        {img: '../images/new/Closet.jpg', img2: 'url("../images/new/Closet.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#about'},
         {img: '../images/new/Fun Old Kitchen Items.jpg', img2: 'url("../images/new/Fun Old Kitchen Items.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#contact'},
         {img: '../images/new/Pink Hues.jpg', img2: 'url("../images/new/Pink Hues.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#donations'},
         {img: '../images/Fun Decor.jpg', img2: 'url("../images/Fun Decor.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#home'},
         {img: '../images/new/Art on a Shelf Mirrored.png', img2: 'url("../images/new/Art on a Shelf Mirrored.png"), url("../images/subtle patterns/crossword.png")', pageId: '#portfolio'},
         {img: '../images/new/Coffee Bar.cropped.jpg', img2: 'url("../images/new/Coffee Bar.cropped.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#rates'},
-        {img: '../images/new/Containers.jpg', img2: 'url("../images/new/Containers.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#resources'},
+        {img: '../images/new/File Folder.jpg', img2: 'url("../images/new/File Folder.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#resources'},
         {img: '../images/new/Living Space.jpg', img2: 'url("../images/new/Living Space.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#services'},
         {img: '../images/new/Shelves and Storage.jpg', img2: 'url("../images/new/Shelves and Storage.jpg"), url("../images/subtle patterns/crossword.png")', pageId: '#whatIsAnOrganizer'},
       ];
